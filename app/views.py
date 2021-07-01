3d3962e14d1f6893fcbdca5f1e36e059692d5e33
Mahi Aminu Aliyu <mahigital@gmail.com>
2021-07-01 09:51:17 +0100

    modified typo

--- app/views.py
+++ app/views.py
@@ -31,7 +31,8 @@ def index(request):
         qr.add_data(f"Name: {other} {surname}\nReg No: {regno}\nEmail Address: {email}\nDepartment: {department} \nPhone No: {phone}")
         qr.make(fit = True)
         img = qr.make_image(fill_color = "black", black_color = "white")
-        img.save(f"{MEDIA_ROOT}/{surname}.png")        
+        png = img.save(f"{MEDIA_ROOT}{surname}.png")
+        print(png)
 
         with open(f"{MEDIA_ROOT}/{surname}.png", "rb") as f:
             data = f.read()
